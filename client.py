d54a4b25c6d1ce8f24a69a06305e4abf6ba8e33c
ADITYA NALANG <adityanalang2004@gmail.com>
2024-12-09 15:25:16 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+ 
+# pip install openai 
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+  api_key="Enter your api key",
+)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message.content)
+
+#"for api call we need a api-key that is not free to used it is paid"
---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "march": "https://www.youtube.com/watch?v=hOHKltAiKXQ",
+    "wolf": "https://www.youtube.com/watch?v=hvKaPTmQBA8",
+    "skyfall": "https://www.youtube.com/watch?v=roz9sXFkTuE&list=PL4fGSI1pDJn40WjZ6utkIuj2rNg-7iGsq",
+    "cat":"https://youtu.be/roz9sXFkTuE?si=1cvl-YgDPYTYu4fn"
+}
